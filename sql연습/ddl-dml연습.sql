d8a6c476d25bfb744a0b8b1e6aac6aa98a53838e
hyojining <gywls1390@gmail.com>
2023-09-01 10:52:06 +0900

    dml 연습

--- sql연습/ddl-dml연습.sql
+++ sql연습/ddl-dml연습.sql
@@ -34,3 +34,36 @@ desc member;
 --
 -- dml
 -- 
+
+-- insert
+insert 
+  into member 
+values (null, 'gywls1390@gmail.com', password('1234'), '김효진', '개발팀', null);
+select * from member;
+
+insert 
+  into member(no, email, password, name, dept, self_intro)
+values (null, 'gywls1390@gmail.com', password('1234'), '김효진2', '개발팀2', null);
+select * from member;
+
+insert 
+  into member(email, password, name, dept)
+values ('gywls1390@gmail.com', password('1234'), '김효진3', '개발팀3');
+select * from member;
+
+insert 
+  into member(email, name, dept,  password)
+values ('gywls1390@gmail.com', '김효진4', '개발팀4', password('1234'));
+select * from member;
+
+-- update
+update member 
+   set email='gywls@gmail.com', name='효진'
+ where no = 4;
+select * from member;
+
+-- delete
+delete 
+  from member 
+ where no = 4;
+select * from member;
