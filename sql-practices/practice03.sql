891f0e90f8f2ebe3efea4edabcc90a790278372c
hyojining <gywls1390@gmail.com>
2023-09-04 09:00:33 +0900

    practice03 update

--- sql-practices/practice03.sql
+++ sql-practices/practice03.sql
@@ -2,7 +2,7 @@
 
 -- 문제 1. 
 -- 현재 급여가 많은 직원부터 직원의 사번, 이름, 그리고 연봉을 출력 하시오.
-select a.emp_no, a.first_name, b.salary
+select a.emp_no, concat(a.first_name, ' ', a.last_name), b.salary
 from employees a, salaries b
 where a.emp_no = b.emp_no
 and b.to_date = '9999-01-01'
@@ -10,7 +10,7 @@ order by b.salary desc;
 
 -- 문제2.
 -- 전체 사원의 사번, 이름, 현재 직책을 이름 순서로 출력하세요.
-select a.emp_no, a.first_name, b.title
+select a.emp_no, concat(a.first_name, ' ', a.last_name), b.title
 from employees a, titles b
 where a.emp_no = b.emp_no
 and b.to_date = '9999-01-01'
@@ -18,7 +18,7 @@ order by a.first_name;
 
 -- 문제3.
 -- 전체 사원의 사번, 이름, 현재 부서를 이름 순서로 출력하세요.
-select a.emp_no, a.first_name, c.dept_name
+select a.emp_no, concat(a.first_name, ' ', a.last_name), c.dept_name
 from employees a, dept_emp b, departments c
 where a.emp_no = b.emp_no
 and b.dept_no = c.dept_no
@@ -27,7 +27,7 @@ order by a.first_name;
 
 -- 문제4. (노가다)
 -- 전체 사원의 사번, 이름, 연봉, 직책, 부서를 모두 이름 순서로 출력합니다.
-select a.emp_no, a.first_name, b.salary, c.title, e.dept_name
+select a.emp_no, concat(a.first_name, ' ', a.last_name), b.salary, c.title, e.dept_name
 from employees a, salaries b, titles c, dept_emp d, departments e
 where a.emp_no = b.emp_no
 and a.emp_no = c.emp_no
@@ -42,7 +42,7 @@ order by a.first_name;
 -- ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요.
 -- 현재 ‘Technique Leader’의 직책으로 근무하는 사원은 고려하지 않습니다.
 -- 이름은 first_name과 last_name을 합쳐 출력 합니다.
-select a.emp_no, concat(a.first_name, ' ', a.last_name) as 이름
+select a.emp_no, concat(a.first_name, ' ', a.last_name)
 from employees a, titles b
 where a.emp_no = b.emp_no
 and b.to_date != '9999-01-01'
@@ -50,7 +50,7 @@ and b.title = 'Technique Leader';
 
 -- 문제6.
 -- 직원 이름(last_name) 중에서 s로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
-select a.last_name, c.dept_name, d.title
+select concat(a.first_name, ' ', a.last_name), c.dept_name, d.title
 from employees a, dept_emp b, departments c, titles d
 where a.emp_no = b.emp_no
 and b.dept_no = c.dept_no
@@ -60,7 +60,7 @@ and a.last_name like 's%';
 -- 문제7.
 -- 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.
 -- 이름, 급여 출력
-select a.first_name, c.salary
+select concat(a.first_name, ' ', a.last_name), c.salary
 from employees a, titles b, salaries c
 where a.emp_no = b.emp_no
 and a.emp_no = c.emp_no
